03c03fde8e8fa5c81ff31ab601326a88b8d039ab
Pritam Acharya <pritamach.exe@gmail.com>
2024-11-19 22:32:34 +0530

    Update AuroraBackground.dart

--- lib/main page/AuroraBackground.dart
+++ lib/main page/AuroraBackground.dart
@@ -5,13 +5,10 @@ class AuroraBackground extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    // Dynamically adjust the number of images for smaller devices
     final isSmallScreen = size.height <= 600;
 
-    // Predefined transparency values
     final List<double> opacities = [0.5, 0.1, 0.3, 0.2, 0.4];
 
-    // Aurora images data (path, width, height, position)
     final auroraImages = [
       {'path': 'assets/pink.png', 'width': 1000.0, 'height': 800.0, 'top': -100.0, 'left': -50.0},
       {'path': 'assets/teal.png', 'width': 1500.0, 'height': 1500.0, 'top': -200.0, 'left': -750.0},
@@ -20,18 +17,14 @@ class AuroraBackground extends StatelessWidget {
       {'path': 'assets/teal.png', 'width': 500.0, 'height': 400.0, 'top': -90.0, 'right': -200.0},
     ];
 
-    // Limit images if on a smaller screen
     final displayedImages = isSmallScreen ? auroraImages.take(3).toList() : auroraImages;
 
-    // Calculate a size scaling factor based on the screen size
     double scaleFactor = isSmallScreen ? 0.6 : 1.0;
 
     return Stack(
       children: [
-        // Main background color
         Container(color: Colors.black),
 
-        // Aurora layers with predefined transparency and scaling
         for (int i = 0; i < displayedImages.length; i++)
           _buildAuroraImage(displayedImages[i], opacities[i], scaleFactor),
       ],
@@ -48,9 +41,9 @@ class AuroraBackground extends StatelessWidget {
         opacity: opacity,
         child: Image.asset(
           imageData['path'],
-          width: imageData['width'] * scaleFactor,  // Scale the width
-          height: imageData['height'] * scaleFactor,  // Scale the height
-          fit: BoxFit.contain, // Ensure the image is fully visible
+          width: imageData['width'] * scaleFactor, 
+          height: imageData['height'] * scaleFactor,  
+          fit: BoxFit.contain, 
         ),
       ),
     );
